752a668949d105f61cef0fd3924e1c5c9f47510f
David Ascher <david.ascher@gmail.com>
2012-02-01 08:47:36 -0800

    buttons to lead the witness

--- assets/js/application.js
+++ assets/js/application.js
@@ -13,13 +13,6 @@
     // make code pretty
     window.prettyPrint && prettyPrint()
 
-    // add-ons
-    $('.add-on :checkbox').on('click', function () {
-      var $this = $(this)
-        , method = $this.attr('checked') ? 'addClass' : 'removeClass'
-      $(this).parents('.add-on')[method]('active')
-    })
-
     // position static twipsies for components page
     if ($(".twipsies a").length) {
       $(window).on('load resize', function () {
@@ -31,14 +24,6 @@
             })
             .tooltip('show')
           })
-      })
-    }
-
-    // add tipsies to grid for scaffolding
-    if ($('#grid-system').length) {
-      $('#grid-system').tooltip({
-          selector: '.show-grid > div'
-        , title: function () { return $(this).width() + 'px' }
       })
     }
 
@@ -63,85 +48,7 @@
       }
     }
 
-    // tooltip demo
-    $('.tooltip-demo.well').tooltip({
-      selector: "a[rel=tooltip]"
-    })
-
-    $('.tooltip-test').tooltip()
-    $('.popover-test').popover()
-
-    // popover demo
-    $("a[rel=popover]")
-      .popover()
-      .click(function(e) {
-        e.preventDefault()
-      })
-
-    // button state demo
-    $('#fat-btn')
-      .click(function () {
-        var btn = $(this)
-        btn.button('loading')
-        setTimeout(function () {
-          btn.button('reset')
-        }, 3000)
-      })
-
-    // carousel demo
     $('.carousel').carousel(  {interval: 100000}).carousel('pause')
-
-    // javascript build logic
-    var inputsComponent = $("#components.download input")
-      , inputsPlugin = $("#plugins.download input")
-      , inputsVariables = $("#variables.download input")
-
-    // toggle all plugin checkboxes
-    $('#components.download .toggle-all').on('click', function (e) {
-      e.preventDefault()
-      inputsComponent.attr('checked', !inputsComponent.is(':checked'))
-    })
-
-    $('#plugins.download .toggle-all').on('click', function (e) {
-      e.preventDefault()
-      inputsPlugin.attr('checked', !inputsPlugin.is(':checked'))
-    })
-
-    $('#variables.download .toggle-all').on('click', function (e) {
-      e.preventDefault()
-      inputsVariables.val('')
-    })
-
-    // request built javascript
-    $('.download-btn').on('click', function () {
-
-      var css = $("#components.download input:checked")
-            .map(function () { return this.value })
-            .toArray()
-        , js = $("#plugins.download input:checked")
-            .map(function () { return this.value })
-            .toArray()
-        , vars = {}
-        , img = ['glyphicons-halflings-sprite.png', 'glyphicons-halflings-sprite-white.png']
-
-    $("#variables.download input")
-      .each(function () {
-        $(this).val() && (vars[ $(this).prev().text() ] = $(this).val())
-      })
-
-      $.ajax({
-        type: 'POST'
-      , url: 'http://bootstrap.herokuapp.com'
-      , dataType: 'jsonpi'
-      , params: {
-          branch: '2.0-wip'
-        , js: js
-        , css: css
-        , vars: vars
-        , img: img
-      }
-      })
-    })
   })
 
 // Modified from the original jsonpi https://github.com/benvinegar/jquery-jsonpi
